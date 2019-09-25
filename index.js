b9600f75ad7fcee4cafb0437dda9086621ec8623
maxlath <k@maxlath.eu>
2019-09-25 17:55:58 +0200

    removed hex and bghex functions

--- index.js
+++ index.js
@@ -1,56 +1,3 @@
-// copy from https://github.com/Qix-/color-convert
-function hex2rgb(args) {
-	const match = args.toString(16).match(/[a-f0-9]{6}|[a-f0-9]{3}/i);
-	if (!match) {
-		return [0, 0, 0];
-	}
-
-	let colorString = match[0];
-
-	if (match[0].length === 3) {
-		colorString = colorString
-			.split("")
-			.map(char => {
-				return char + char;
-			})
-			.join("");
-	}
-
-	const integer = parseInt(colorString, 16);
-	const r = (integer >> 16) & 0xff;
-	const g = (integer >> 8) & 0xff;
-	const b = integer & 0xff;
-
-	return [r, g, b];
-}
-
-// copy from https://github.com/Qix-/color-convert
-function rgb2ansi256(args) {
-	const r = args[0];
-	const g = args[1];
-	const b = args[2];
-
-	if (r === g && g === b) {
-		if (r < 8) {
-			return 16;
-		}
-
-		if (r > 248) {
-			return 231;
-		}
-
-		return Math.round(((r - 8) / 247) * 24) + 232;
-	}
-
-	const ansi =
-		16 +
-		36 * Math.round((r / 255) * 5) +
-		6 * Math.round((g / 255) * 5) +
-		Math.round((b / 255) * 5);
-
-	return ansi;
-}
-
 // copy from https://github.com/chalk/chalk
 const styleMap = {
 	// style
@@ -102,18 +49,6 @@ const styleMap = {
 
 // write by myself
 function Chalk() {}
-
-Chalk.prototype.hex = function(hex) {
-	return function(content) {
-		return `\u001B[38;5;${rgb2ansi256(hex2rgb(hex))}m${content}\u001B[39m`;
-	};
-};
-
-Chalk.prototype.bghex = function(hex) {
-	return function(content) {
-		return `\u001B[48;5;${rgb2ansi256(hex2rgb(hex))}m${content}\u001B[49m`;
-	};
-};
 
 const styleName = Object.keys(styleMap);
 for(let i=0, len=styleName.length; i<len; i++){
